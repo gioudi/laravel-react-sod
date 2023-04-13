fb6c0586061ed68df93fb7ecaa815fce385fbde6
gioudi <sergiopenagos881@gmail.com>
2023-04-13 18:55:07 -0500

    feat/install_inertia

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
-use App\Category;
-use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,111 +16,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/* Custom Auth */
-
 Route::get('/', function () {
-    return redirect('/login');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
 
-Auth::routes();
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/forgot_password', 'Auth\ForgotPasswordController@index')->name('forgot.password.get');
-Route::post('/forgot_password', 'Auth\ForgotPasswordController@sendEmail')->name('forgot.password.post');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-
-Route::get('/password/reset/{token}', 'Auth\ResetPasswordController@index')->name('password.reset');
-Route::post('/password/reset', 'Auth\ResetPasswordController@updated')->name('password.update');
-
-/**********/
-
-
-/* Sales */
-
-Route::resource('sales', 'SaleController')->names('sales')->except([
-    'edit', 'update', 'destroy'
-]);
-
-
-
-Route::post('sales/report_results', 'ReportController@report_results')->name('report.results');
-
-Route::get('sales/pdf/{sale}', 'SaleController@pdf')->name('sales.pdf');
-Route::get('sales/print/{sale}', 'SaleController@print')->name('sales.print');
-
-/********/
-
-/* Bussiness */
-
-Route::resource('business', 'BusinessController')->names('business')->only([
-    'index', 'update'
-]);
-
-/**********/
-
-/* Printer */
-
-Route::resource('printers', 'PrinterController')->names('printers')->only([
-    'index', 'update'
-]);
-
-/**********/
-
-
-/* Category */
-
-Route::resource('categories', 'CategoryController')->names('categories');
-
-/**********/
-
-
-/* Client */
-
-Route::resource('clients', 'ClientController')->names('clients');
-
-/*********/
-
-
-/* Product */
-
-Route::resource('products', 'ProductController')->names('products');
-
-/*********/
-
-
-/* Providers */
-Route::resource('providers', 'ProviderController')->names('providers');
-
-/***********/
-
-
-/* Purchase */
-
-Route::resource('purchases', 'PurchaseController')->names('purchases')->except([
-    'edit', 'update',
-]);
-
-Route::get('purchases/pdf/{purchase}', 'PurchaseController@pdf')->name('purchases.pdf');
-Route::get('purchases/upload/{purchase}', 'PurchaseController@upload')->name('upload.purchases');
-
-/*********/
-
-
-/* Change */
-
-Route::get('change_status/products/{product}', 'ProductController@change_status')->name('change.status.products');
-Route::get('change_status/purchases/{purchase}', 'PurchaseController@change_status')->name('change.status.purchases');
-Route::get('change_status/sales/{sale}', 'SaleController@change_status')->name('change.status.sales');
-
-/********/
-
-/* Users and Roles */
-
-Route::resource('users', 'UserController')->names('users');
-Route::resource('roles', 'RoleController')->names('roles');
-
-/**************/
-
-Route::get('reports_day', 'ReportController@reports_day')->name('reports.day');
-Route::get('reports_date', 'ReportController@reports_date')->name('reports.date');
-
-Route::get('/home', 'HomeController@index')->name('home');
+require __DIR__.'/auth.php';
