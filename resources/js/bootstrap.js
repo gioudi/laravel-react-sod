0f004aa20fcf353e8e78962c3f3a082e55e018a7
gioudi <sergiopenagos881@gmail.com>
2023-04-14 16:06:35 -0500

    feat/migrate_vite_from_laravel_mix

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,5 +1,6 @@
-window._ = require('lodash');
 
+import _ from 'lodash'
+window._ = _;
 /**
  * We'll load jQuery and the Bootstrap jQuery plugin which provides support
  * for JavaScript based Bootstrap features such as modals and tabs. This
@@ -18,8 +19,9 @@ try {
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
+import axios from 'axios'
 
-window.axios = require('axios');
+window.axios =  axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
@@ -31,11 +33,12 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 // import Echo from 'laravel-echo';
 
-// window.Pusher = require('pusher-js');
+// import Pusher from 'pusher-js';
+// window.Pusher = Pusher;
 
 // window.Echo = new Echo({
 //     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+//     key: process.env.VITE_PUSHER_APP_KEY,
+//     cluster: process.env.VITE_PUSHER_APP_CLUSTER,
 //     forceTLS: true
 // });
