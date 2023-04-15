f2d437da48a3de2e6dde83cff4d354440adb8070
gioudi <sergiopenagos881@gmail.com>
2023-04-14 22:20:55 -0500

    feat/setup_eslint

--- resources/js/Components/InputLabel.tsx
+++ resources/js/Components/InputLabel.tsx
@@ -1,7 +1,15 @@
-export default function InputLabel({ value, className = '', children, ...props }) {
-    return (
-        <label {...props} className={`block font-medium text-sm text-gray-700 ` + className}>
-            {value ? value : children}
-        </label>
-    );
+export default function InputLabel({
+  value,
+  className = '',
+  children,
+  ...props
+}) {
+  return (
+    <label
+      {...props}
+      className={`block font-medium text-sm text-gray-700 ` + className}
+    >
+      {value ? value : children}
+    </label>
+  );
 }
--- resources/js/Pages/Auth/ResetPassword.tsx
+++ resources/js/Pages/Auth/ResetPassword.tsx
@@ -7,88 +7,91 @@ import TextInput from '@/Components/TextInput';
 import { Head, useForm } from '@inertiajs/react';
 
 export default function ResetPassword({ token, email }) {
-    const { data, setData, post, processing, errors, reset } = useForm({
-        token: token,
-        email: email,
-        password: '',
-        password_confirmation: '',
-    });
+  const { data, setData, post, processing, errors, reset } = useForm({
+    token: token,
+    email: email,
+    password: '',
+    password_confirmation: '',
+  });
 
-    useEffect(() => {
-        return () => {
-            reset('password', 'password_confirmation');
-        };
-    }, []);
-
-    const onHandleChange = (event) => {
-        setData(event.target.name, event.target.value);
+  useEffect(() => {
+    return () => {
+      reset('password', 'password_confirmation');
     };
+  }, []);
 
-    const submit = (e) => {
-        e.preventDefault();
+  const onHandleChange = event => {
+    setData(event.target.name, event.target.value);
+  };
 
-        post(route('password.store'));
-    };
+  const submit = e => {
+    e.preventDefault();
 
-    return (
-        <GuestLayout>
-            <Head title="Reset Password" />
+    post(route('password.store'));
+  };
 
-            <form onSubmit={submit}>
-                <div>
-                    <InputLabel htmlFor="email" value="Email" />
+  return (
+    <GuestLayout>
+      <Head title="Reset Password" />
 
-                    <TextInput
-                        id="email"
-                        type="email"
-                        name="email"
-                        value={data.email}
-                        className="mt-1 block w-full"
-                        autoComplete="username"
-                        onChange={onHandleChange}
-                    />
+      <form onSubmit={submit}>
+        <div>
+          <InputLabel htmlFor="email" value="Email" />
 
-                    <InputError message={errors.email} className="mt-2" />
-                </div>
+          <TextInput
+            id="email"
+            type="email"
+            name="email"
+            value={data.email}
+            className="mt-1 block w-full"
+            autoComplete="username"
+            onChange={onHandleChange}
+          />
 
-                <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password" />
+          <InputError message={errors.email} className="mt-2" />
+        </div>
 
-                    <TextInput
-                        id="password"
-                        type="password"
-                        name="password"
-                        value={data.password}
-                        className="mt-1 block w-full"
-                        autoComplete="new-password"
-                        isFocused={true}
-                        onChange={onHandleChange}
-                    />
+        <div className="mt-4">
+          <InputLabel htmlFor="password" value="Password" />
 
-                    <InputError message={errors.password} className="mt-2" />
-                </div>
+          <TextInput
+            id="password"
+            type="password"
+            name="password"
+            value={data.password}
+            className="mt-1 block w-full"
+            autoComplete="new-password"
+            isFocused={true}
+            onChange={onHandleChange}
+          />
 
-                <div className="mt-4">
-                    <InputLabel htmlFor="password_confirmation" value="Confirm Password" />
+          <InputError message={errors.password} className="mt-2" />
+        </div>
 
-                    <TextInput
-                        type="password"
-                        name="password_confirmation"
-                        value={data.password_confirmation}
-                        className="mt-1 block w-full"
-                        autoComplete="new-password"
-                        onChange={onHandleChange}
-                    />
+        <div className="mt-4">
+          <InputLabel
+            htmlFor="password_confirmation"
+            value="Confirm Password"
+          />
 
-                    <InputError message={errors.password_confirmation} className="mt-2" />
-                </div>
+          <TextInput
+            type="password"
+            name="password_confirmation"
+            value={data.password_confirmation}
+            className="mt-1 block w-full"
+            autoComplete="new-password"
+            onChange={onHandleChange}
+          />
 
-                <div className="flex items-center justify-end mt-4">
-                    <PrimaryButton className="ml-4" disabled={processing}>
-                        Reset Password
-                    </PrimaryButton>
-                </div>
-            </form>
-        </GuestLayout>
-    );
+          <InputError message={errors.password_confirmation} className="mt-2" />
+        </div>
+
+        <div className="flex items-center justify-end mt-4">
+          <PrimaryButton className="ml-4" disabled={processing}>
+            Reset Password
+          </PrimaryButton>
+        </div>
+      </form>
+    </GuestLayout>
+  );
 }
